79e9e35473a9ae8dabc7a644b75487a6734c2512
AirTheseus <airtheseusgungun@outlook.com>
2025-04-19 17:37:46 +0800

    Update P1009.cpp

--- P1009.cpp
+++ P1009.cpp
@@ -7,4 +7,20 @@ using std::cout;
 int main()
 {
     int num;
+    size_t result = 0;
+	std::vector<size_t> numList(51, 1);
+
+	cin >> num;
+
+	for (int i = 2; i <= num; i++)
+	{
+		numList[i] = i * numList[i - 1];
+	}
+
+	for (int j = 1; j <= num; j++)
+	{
+		result += numList[j];
+	}
+
+	cout << result;
 }
